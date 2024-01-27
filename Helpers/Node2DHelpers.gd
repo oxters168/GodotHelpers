945bbe61ff5fc9684a6495714c012c22d57acc3e
Oxters <oxters168@gmail.com>
2024-01-26 21:18:21 -0500

    Added Node2DHelpers

---
+++ Helpers/Node2DHelpers.gd
@@ -0,0 +1,35 @@
+class_name Node2DHelpers
+
+## Returns a vector representing the global right direction of the given object
+## Shorthand for node.get_global_transform_with_canvas().basis.x
+static func get_global_right(node: Node) -> Vector2:
+	return node.get_global_transform_with_canvas().x
+## Returns a vector representing the global up direction of the given object
+## Shorthand for node.get_global_transform_with_canvas().basis.y
+static func get_global_up(node: Node) -> Vector2:
+	return node.get_global_transform_with_canvas().y
+## Returns a vector representing the global left direction of the given object
+## Shorthand for -node.get_global_transform_with_canvas().basis.x
+static func get_global_left(node: Node) -> Vector2:
+	return -node.get_global_transform_with_canvas().x
+## Returns a vector representing the global down direction of the given object
+## Shorthand for -node.get_global_transform_with_canvas().basis.y
+static func get_global_down(node: Node) -> Vector2:
+	return -node.get_global_transform_with_canvas().y
+
+## Returns a vector representing the local right direction of the given object
+## Shorthand for node.transform.basis.x
+static func get_local_right(node: Node) -> Vector2:
+	return node.transform.basis.x
+## Returns a vector representing the local up direction of the given object
+## Shorthand for node.transform.basis.y
+static func get_local_up(node: Node) -> Vector2:
+	return node.transform.basis.y
+## Returns a vector representing the local left direction of the given object
+## Shorthand for -node.transform.basis.x
+static func get_local_left(node: Node) -> Vector2:
+	return -node.transform.basis.x
+## Returns a vector representing the local down direction of the given object
+## Shorthand for -node.transform.basis.y
+static func get_local_down(node: Node) -> Vector2:
+	return -node.transform.basis.y
